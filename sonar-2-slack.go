e98b23bdc0f25eb3e1f2506cbc307b3ed72fe0d0
Nhat Hoang <nhat.hd@geekup.vn>
2018-10-06 11:05:35 +0700

    update metrics

--- sonar-2-slack.go
+++ sonar-2-slack.go
@@ -19,7 +19,6 @@ type ServerInfor struct {
 	SonarURL     string `env:"SONAR_URL"`
 	SlackHookURL string `env:"SLACK_HOOK_URL"`
 	ProjectName  string `env:"PROJECT_ALIAS_NAME"`
-	SlackChanel  string `env:"SLACK_CHANNEL" envDefault:"#general"`
 }
 
 // SonarStatus Response from json
@@ -47,7 +46,10 @@ type NotifContent struct {
 	Bugs                   string
 	DuplicatedLinesDensity string
 	CodeSmells             string
-	Coverage 	      			 string
+	Coverage               string
+	Tests                  string
+	TestsFailed            string
+	TestSuccessRatio       string
 }
 
 // Field slack
@@ -87,10 +89,9 @@ type Payload struct {
 }
 
 func fetchState(serverInfor ServerInfor) *resty.Response {
-	resq, err := resty.
-		R().
+	resq, err := resty.R().
 		SetQueryParams(map[string]string{
-			"metricKeys":   "bugs, duplicated_lines_density, code_smells, alert_status, sqale_index, coverage",
+			"metricKeys":   "bugs, duplicated_lines_density, code_smells, alert_status, sqale_index, coverage, tests, test_errors, test_success_density",
 			"componentKey": serverInfor.ProjectName,
 		}).
 		SetBasicAuth(serverInfor.Username, serverInfor.Password).
@@ -98,6 +99,7 @@ func fetchState(serverInfor ServerInfor) *resty.Response {
 	if err != nil {
 		return nil
 	}
+	fmt.Println(resq)
 	return resq
 }
 
@@ -138,6 +140,12 @@ func convertToNotif(status SonarStatus) (content NotifContent) {
 			notifContent.DuplicatedLinesDensity = val.Value
 		case "coverage":
 			notifContent.Coverage = val.Value
+		case "tests":
+			notifContent.Tests = val.Value
+		case "test_errors":
+			notifContent.TestsFailed = val.Value
+		case "test_success_density":
+			notifContent.TestSuccessRatio = val.Value
 
 		}
 	}
@@ -147,9 +155,12 @@ func convertToNotif(status SonarStatus) (content NotifContent) {
 func (notifContent NotifContent) text() string {
 	return notifContent.Status + " \n *" +
 		notifContent.Bugs + " bugs*\n Technical debt: *" + notifContent.SqaleIndex +
-		" days*\n Duplicated: *" + notifContent.DuplicatedLinesDensity +
-		"%* \n *" + notifContent.CodeSmells + "* Code Smells" + 
-		"%* \n *" + notifContent.Coverage + "* Code coverage"
+		" days*\n Duplicated: " + notifContent.DuplicatedLinesDensity +
+		"\n *" + notifContent.CodeSmells + "* Code Smells" +
+		"\n *" + notifContent.Coverage +"%"+ "* Code coverage" +
+		"\n *" + notifContent.Tests + "* Tests executed" +
+		"\n *" + notifContent.TestsFailed + "* Tests has failed" +
+		"\n *" + notifContent.TestSuccessRatio +"%"+ "* Tests were success "
 
 }
 
@@ -162,10 +173,9 @@ func manualSendSlack(serverInfor ServerInfor, notifContent NotifContent) {
 		MarkDownIn: []string{"text"},
 	}
 	payload := Payload{
-		Channel:     serverInfor.SlackChanel,
 		Attachments: []Attachment{attachment},
-		Username:    "CI-Bot",
-		IconEmoji:   ":monkey_face:",
+		Username:    "Android CI",
+		IconEmoji:   ":android:",
 	}
 	resp, err := resty.R().SetBody(payload).Post(serverInfor.SlackHookURL)
 	fmt.Println(resp, err)
